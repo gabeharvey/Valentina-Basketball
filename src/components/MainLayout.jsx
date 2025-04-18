66468cd101023293c8bf8f3a1fa2ebad6602f0a1
Gabe Harvey <harvey7@gmail.com>
2025-04-18 13:00:01 -0500

    Minor Code Cleanup

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,12 +1,10 @@
 import PlayerBio from './PlayerBio';
 import PlayerCards from './PlayerCards';
 import Contact from './Contact';
-// import StatTracker from './StatTracker';
 import HoopsFilm from './HoopsFilm';
 // import Offers from './Offers';
-import Media from './Media';
+// import Media from './Media';
 import Academics from './Academics';
-// import AthleteProgression from './AthleteProgression';
 
 const MainLayout = () => {
   return (
@@ -20,15 +18,9 @@ const MainLayout = () => {
       <div id="player-cards">
         <PlayerCards />
       </div>
-      {/* <div id="athlete-progression">
-        <AthleteProgression />
-      </div> */}
-      {/* <div id="stat-tracker">
-        <StatTracker />
-      </div> */}
-      <div id="media">
+      {/* <div id="media">
         <Media />
-      </div>
+      </div> */}
       <div id="academics">
         <Academics />
       </div>
